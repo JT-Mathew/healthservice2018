d7625f38ae98302e4dc257c05288add6920b4324
Hayeon Kim <hayeon.kim1379@gmail.com>
2018-10-02 16:25:45 +1000

    test 2

--- app/src/main/java/c/hayeon/seprroject/LoginActivity.java
+++ app/src/main/java/c/hayeon/seprroject/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         test = new User("Hayeon", "Kim", "1",
                 "1", "46", "Raleigh Avenue",
                 "Caringbah", 2229);
-
+//asdfasdfadsf
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
