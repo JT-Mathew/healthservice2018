d647ee688f32b108bff6f0f6a4d341a6864559da
Hayeon Kim <hayeon.kim1379@gmail.com>
2018-10-02 16:25:31 +1000

    test 2

--- app/src/main/java/c/hayeon/seprroject/LoginActivity.java
+++ app/src/main/java/c/hayeon/seprroject/LoginActivity.java
@@ -33,7 +33,6 @@ public class LoginActivity extends AppCompatActivity {
                 "1", "46", "Raleigh Avenue",
                 "Caringbah", 2229);
 
-//blahfghfghj
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
